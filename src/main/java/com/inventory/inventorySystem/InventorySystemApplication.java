baa0fde666061ee7ea72749663bc6d9bc6949365
OliveroJ16 <jose.olivero.uni@gmail.com>
2025-07-21 00:05:50 -0500

    refactor(config): actualizar carga de variables de entorno y eliminar código innecesario

--- src/main/java/com/inventory/inventorySystem/InventorySystemApplication.java
+++ src/main/java/com/inventory/inventorySystem/InventorySystemApplication.java
@@ -1,6 +1,5 @@
 package com.inventory.inventorySystem;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InventorySystemApplication {
 
 	public static void main(String[] args) {
-		try {
-			//cargar variables de entorno
-			Dotenv.configure().systemProperties().load();
-			System.out.println("Archivo cargado y variables expuestas");
-		} catch (Exception e) {
-			System.err.println("Error al cargar el archivo .env: " + e.getMessage());
-		}
 		SpringApplication.run(InventorySystemApplication.class, args);
 	}
 
