f31e9c89a7150c85bf035a8351a299227ed89a7d
OliveroJ16 <jose.olivero.uni@gmail.com>
2025-07-30 17:53:06 -0500

    feat: Implementar proveedor de tokens JWT
    Añade la clase JwtTokenProvider para manejar la generación, validación y extracción de claims de JWT.
    Incluye la generación de tokens de acceso y refresco, y la gestión segura de claves.

--- src/main/java/com/inventory/inventorySystem/security/JwtTokenProvider.java
+++ src/main/java/com/inventory/inventorySystem/security/JwtTokenProvider.java
@@ -1,6 +1,8 @@
 package com.inventory.inventorySystem.security;
 
 import com.inventory.inventorySystem.dto.response.UserResponse;
+import com.inventory.inventorySystem.model.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -9,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
-import java.util.Map;
+import java.util.HashMap;
 
 @Component
 public class JwtTokenProvider {
@@ -22,7 +24,7 @@ public class JwtTokenProvider {
     @Value("${jwt.refresh-expiration}")
     private long refreshExpiration;
 
-    public String generateToken(final UserResponse user){
+    public String generateAccessToken(final UserResponse user){
         return buildToken(user, jwtExpiration);
     }
 
@@ -30,10 +32,14 @@ public class JwtTokenProvider {
         return buildToken(user, refreshExpiration);
     }
 
-    public String buildToken(UserResponse user, final long expiration){
+    private String buildToken(UserResponse user, final long expiration){
+        var claims = new HashMap<String, Object>();
+        claims.put("userId", user.id());
+        claims.put("name", user.fullName());
+        claims.put("email", user.email());
+        claims.put("role", user.role().name());
         return Jwts.builder()
-                .id(user.id().toString())
-                .claims(Map.of("name", user.fullName()))
+                .claims(claims)
                 .subject(user.email())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + expiration))
@@ -41,8 +47,35 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    public SecretKey getSignInKey(){
+    private SecretKey getSignInKey(){
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = extractClaimsFromToken(token);
+        return claims.getSubject();
+    }
+
+    public boolean isTokenValid(final String token, final User user){
+        String userEmail = getUsernameFromToken(token);
+        return (userEmail.equals(user.getEmail()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        Claims claims = extractClaimsFromToken(token);
+        return claims.getExpiration();
+    }
+
+    private Claims extractClaimsFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
