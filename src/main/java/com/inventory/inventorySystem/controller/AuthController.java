6ea719bcb80b77c4fb01f673be79575cd285ed19
OliveroJ16 <jose.olivero.uni@gmail.com>
2025-07-31 18:50:21 -0500

    feat(auth): Implementa endpoints de logout y refresh token
    Añade los endpoints `/api/v1/auth/logout` para la revocación de tokens y
    `/api/v1/auth/refresh` para la emisión de nuevos tokens de acceso.

--- src/main/java/com/inventory/inventorySystem/controller/AuthController.java
+++ src/main/java/com/inventory/inventorySystem/controller/AuthController.java
@@ -4,21 +4,18 @@ import com.inventory.inventorySystem.dto.request.LoginRequest;
 import com.inventory.inventorySystem.dto.request.RegisterRequest;
 import com.inventory.inventorySystem.dto.response.AuthResponse;
 import com.inventory.inventorySystem.service.interfaces.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController {
-    private final AuthService authService;
 
-    public AuthController(AuthService authService){
-        this.authService = authService;
-    }
+    private final AuthService authService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> registerUser(@RequestBody RegisterRequest registerRequest) {
@@ -29,6 +26,18 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> loginUser(@RequestBody LoginRequest loginRequest){
         var authResponse = authService.loginUser(loginRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(authResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(authResponse);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+        var authResponse = authService.refreshToken(authHeader);
+        return ResponseEntity.status(HttpStatus.OK).body(authResponse);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logoutUser(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+        authService.logout(authHeader);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
